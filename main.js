d4a236afd30e18364c8ab2ad84d8630bdeaa3f96
Rafael Benavides <rabenter@gmail.com>
2017-11-10 20:13:34 +0100

    Proyect initialization.

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+const path = require('path');
+const url = require('url');
+
+let win;
+
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width: 500,
+        height: 400
+    })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashed: true
+    }))
+});
