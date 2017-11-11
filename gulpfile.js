92e3cf48bc566d12863f0b3f07065ffe5a9042de
Rafael Benavides Terrón <rabenter@gmail.com>
2017-11-11 22:44:46 +0100

    - Added js minifier. - Added css minifier. - Added img minifier.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const minifyJs = require('gulp-minify');
+const minifyCss = require('gulp-clean-css');
+const minifyImg = require('gulp-imagemin');
 
 /**
  * Task that execute a local web server precompiling scss files.
@@ -12,16 +15,42 @@ gulp.task('serve', ['sass'], () => {
         files: './views'
     });
 
+    gulp.watch('resources/js/*.js', ['minifyJs']);
+    // gulp.watch('resources/js/*.js', ['minifyCss']);
     gulp.watch('resources/sass/**/*.scss', ['sass']);
     gulp.watch('./views/*.html').on('change', browserSync.reload);
-})
+});
 
 /**
- * Task that precompile modified scss files.
+ * Task that minify js files.
+ */
+gulp.task('minifyJs', () => {
+    gulp.src('resources/js/*.js')
+      .pipe(minifyJs({
+          ext: {
+              src: '.js',
+              min: '.min.js'
+          }
+      }))
+      .pipe(gulp.dest('resources/js/min'));
+  });
+
+/**
+ * Task that precompile and minify scss files.
  */
 gulp.task('sass', () => {
-    return gulp.src('resources/sass/**/*.scss')
+    gulp.src('resources/sass/**/*.scss')
         .pipe(sass())
+        .pipe(minifyCss())
         .pipe(gulp.dest('resources/css'))
         .pipe(browserSync.stream());
+});
+
+/**
+ * Task that minify png, jpg, gif and svg files.
+ */
+gulp.task('minifyImg', () => {
+    gulp.src('resources/img/*')
+    .pipe(minifyImg())
+    .pipe(gulp.dest('resources/img/min'));
 });
