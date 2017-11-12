ce53c04f8d7194abc0dc12ea6113180f643a7623
Rafael Benavides Terrón <rabenter@gmail.com>
2017-11-12 13:56:59 +0100

    - Estructural changes on style folders. - Minor changes on windows appearance.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,10 @@
-const remote = require('electron').remote
-const main = remote.require('./main.js')
+// const remote = require('electron').remote
+// const main = remote.require('./main.js')
 
-let button = document.createElement('button')
-button.textContent = "Ventana"
-document.body.appendChild(button)
+// let button = document.createElement('button')
+// button.textContent = "Ventana"
+// document.body.appendChild(button)
 
-button.addEventListener('click', () => {
-    main.createWindow(100, 100, "index.html");
-})
+// button.addEventListener('click', () => {
+//     main.createWindow(100, 100, "index.html");
+// })
