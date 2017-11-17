a6ab5e60a9082440d892a6c8a4f3387495326724
Rafael Benavides Terrón <rabenter@gmail.com>
2017-11-17 23:30:12 +0100

    - Added eslint into the proyect. - Added background image. - Minor changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,88 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const minifyJs = require('gulp-minify');
-const minifyCss = require('gulp-clean-css');
-const minifyImg = require('gulp-imagemin');
-const minifyHtml = require('gulp-htmlmin');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const minifyJs = require('gulp-minify')
+const minifyCss = require('gulp-clean-css')
+const minifyImg = require('gulp-imagemin')
+const minifyHtml = require('gulp-htmlmin')
+const autoprefixer = require('gulp-autoprefixer')
 const electronServer = require('electron-connect').server.create({
-    stopOnClose: true
-});
+  stopOnClose: true
+})
 
 /**
- * 
+ *
  */
 gulp.task('default', ['compilers', 'watchers'], () => {
-    electronServer.start((state) => {
-        if (state == 'stopped') {
-            process.exit();
-        }
-    });
-});
+  electronServer.start((state) => {
+    if (state === 'stopped') {
+      process.exit()
+    }
+  })
+})
 
 /**
  * Task that runs every watcher.
  */
 gulp.task('watchers', () => {
-    gulp.watch('./src/js/**/*.js', ['compile:js', electronServer.restart]);
-    gulp.watch('./src/sass/**/*.scss', ['compile:css', electronServer.restart]);
-    gulp.watch('./src/views/**/*.html', ['compile:html', electronServer.restart]);
-});
+  gulp.watch('./src/js/**/*.js', ['compile:js', electronServer.restart])
+  gulp.watch('./src/sass/**/*.scss', ['compile:css', electronServer.restart])
+  gulp.watch('./src/views/**/*.html', ['compile:html', electronServer.restart])
+})
 
 /**
  * Task that runs every compiler.
  */
-gulp.task('compilers', ['compile:css', 'compile:js', 'compile:html']);
+gulp.task('compilers', ['compile:css', 'compile:js', 'compile:html'])
 
 /**
  * Task that minify js files.
  */
 gulp.task('compile:js', () => {
-    gulp.src('./src/js/**/*.js')
-        .pipe(minifyJs({
-            ext: {
-                src: '.js',
-                min: '.min.js'
-            }
-        }))
-        .pipe(gulp.dest('./dist/js'));
-  });
+  gulp.src('./src/js/**/*.js')
+    .pipe(minifyJs({
+      ext: {
+        src: '.js',
+        min: '.min.js'
+      }
+    }))
+    .pipe(gulp.dest('./dist/js'))
+})
 
 /**
  * Task that precompile and minify scss files.
  */
 gulp.task('compile:css', () => {
-    gulp.src('./src/sass/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        // .pipe(sass({
-        //     includePaths: [
-        //         './src/sass/abstracts'
-        //     ]
-        // }).on('error', sass.logError))
-        .pipe(minifyCss())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./dist/css'));
-});
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+  // .pipe(sass({
+  //     includePaths: [
+  //         './src/sass/abstracts'
+  //     ]
+  // }).on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./dist/css'))
+})
 
 /**
  * Task that minify png, jpg, gif and svg files.
  */
 gulp.task('compile:img', () => {
-    gulp.src('./src/img/*')
-        .pipe(minifyImg())
-        .pipe(gulp.dest('./dist/img'));
-});
+  gulp.src('./src/img/*')
+    .pipe(minifyImg())
+    .pipe(gulp.dest('./dist/img'))
+})
 
 /**
  * Task that minify hmlt files.
  */
 gulp.task('compile:html', () => {
-    gulp.src('./src/views/**/*.html')
-        .pipe(minifyHtml({
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest('./dist/views'));
-});
+  gulp.src('./src/views/**/*.html')
+    .pipe(minifyHtml({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./dist/views'))
+})
--- main.js
+++ main.js
@@ -1,72 +1,73 @@
-const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const electron = require('electron')
+const {app, BrowserWindow} = electron
 const electronClient = require('electron-connect').client
 
-const path = require('path');
-const url = require('url');
+const path = require('path')
+const url = require('url')
 
 /**
  * Main frame.
  */
-let winClient;
-let win;
+let win
 
 /**
  * Create a new window.
- * 
- * @param {int} width 
- * @param {int} height 
- * @param {string} view 
- * @param {boolean} debug 
+ *
+ * @param {int} width
+ * @param {int} height
+ * @param {string} view
+ * @param {boolean} debug
  */
 exports.createWindow = (width, height, view, debug) => {
-    var frame = new BrowserWindow({
-        minWidth: width,
-        width: width,
-        minHeight: height,
-        height: height,
-        frame: false,
-        resizable: true,
-        show: false,
-        icon: './dist/img/icon.ico'
-    });
-    
-    if (view != undefined) {
-        frame.loadURL(url.format({
-            pathname: path.join(__dirname, `dist/views/${view}.html`),
-            protocol: 'file',
-            slashed: true
-        }));
-    }
+  var frame = new BrowserWindow({
+    minWidth: width,
+    width: width,
+    minHeight: height,
+    height: height,
+    frame: false,
+    resizable: true,
+    show: false,
+    webPreferences: {
+      experimentalFeatures: true
+    },
+    icon: './dist/img/icon.ico'
+  })
 
-    frame.on('close', () => {
-        frame = null;
-        // winClient.sendMessage('close');
-    });
+  if (view !== undefined) {
+    frame.loadURL(url.format({
+      pathname: path.join(__dirname, `dist/views/${view}.html`),
+      protocol: 'file',
+      slashed: true
+    }))
+  }
 
-    if (debug) frame.webContents.openDevTools();
-    
-    return frame;
+  frame.on('close', () => {
+    frame = null
+  })
+
+  if (debug) frame.webContents.openDevTools()
+
+  return frame
 }
 
 /**
  * Once the application is fully loaded, the index frame is executed.
  */
 app.on('ready', () => {
-    win = exports.createWindow(1000, 600, 'index', true);
-    winClient = electronClient.create(win);
+  win = exports.createWindow(1000, 600, 'index', true)
+  electronClient.create(win)
 
-    /**
+  /**
      * The window is shown as soos as it's fully loaded.
      */
-    win.on('ready-to-show', () => {
-        win.show();
-    })
+  win.on('ready-to-show', () => {
+    win.show()
+  })
 
-    /**
+  /**
      * The application will close when window closes event.
      */
-    win.on('closed', () => {
-        app.quit();
-    });
-});
+  win.on('closed', () => {
+    app.quit()
+  })
+})
